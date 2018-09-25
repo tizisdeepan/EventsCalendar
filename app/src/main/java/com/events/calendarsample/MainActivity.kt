940c500ee5c18edc41bc9cbb622d0b3c453408e3
Deepan <deepan.e@zohocorp.com>
2018-09-25 20:53:48 +0530

    Event Dots, Event Calendat Utils, Customisation Options

--- app/src/main/java/com/events/calendarsample/MainActivity.kt
+++ app/src/main/java/com/events/calendarsample/MainActivity.kt
@@ -14,11 +14,14 @@ class MainActivity : AppCompatActivity(), EventsCalendar.Callback {
 
     override fun onDaySelected(selectedDate: Calendar?) {
         Log.e("DAY", "SELECTED")
+        selected.text = getDateString(selectedDate?.timeInMillis)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selected.text = getDateString(eventsCalendar.getCurrentSelectedDate()?.timeInMillis)
 
         val today = Calendar.getInstance()
         val end = Calendar.getInstance()
@@ -33,11 +36,42 @@ class MainActivity : AppCompatActivity(), EventsCalendar.Callback {
         eventsCalendar.setCallback(this)
 
         val c = Calendar.getInstance()
-        c.add(Calendar.DAY_OF_MONTH, 6)
+        c.add(Calendar.DAY_OF_MONTH, 2)
+        eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 3)
+        eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 4)
+        eventsCalendar.addEvent(c)
+        c.add(Calendar.DAY_OF_MONTH, 7)
         eventsCalendar.addEvent(c)
 
-        text.setOnClickListener {
-            
+        selected.setOnClickListener {
+            eventsCalendar.setCurrentSelectedDate(eventsCalendar.getCurrentSelectedDate())
         }
+
+        selected.typeface = FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, this)
+    }
+
+    fun getDateString(time: Long?): String {
+        if (time != null) {
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = time
+            val month = when (cal[java.util.Calendar.MONTH]) {
+                Calendar.JANUARY -> "January"
+                Calendar.FEBRUARY -> "February"
+                Calendar.MARCH -> "March"
+                Calendar.APRIL -> "April"
+                Calendar.MAY -> "May"
+                Calendar.JUNE -> "June"
+                Calendar.JULY -> "July"
+                Calendar.AUGUST -> "August"
+                Calendar.SEPTEMBER -> "September"
+                Calendar.OCTOBER -> "October"
+                Calendar.NOVEMBER -> "November"
+                Calendar.DECEMBER -> "December"
+                else -> ""
+            }
+            return "$month ${cal[Calendar.DAY_OF_MONTH]}, ${cal[Calendar.YEAR]}"
+        } else return ""
     }
 }
